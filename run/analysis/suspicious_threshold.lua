d00f369c2f8449cccb20e2d08bd497099fbe5ee5
Julien Vehent <julien@linuxwall.info>
2017-01-21 00:29:01 -0500

    Improve suspicious threshold analyzer

--- run/analysis/suspicious_threshold.lua
+++ run/analysis/suspicious_threshold.lua
@@ -10,6 +10,9 @@ local xss = '((\%3C)|<)((\%2F)|\/)*[a-z0-9\%]+((\%3E)|>)'
 -- for a given IP are garbage collected.
 local client_stats = {}
 
+local client_violations = {}
+local threshold = read_config("violations_threshold")
+
 function ipv4_str_to_int(ipstr)
   local o1,o2,o3,o4 = ipstr:match("(%d+)%.(%d+)%.(%d+)%.(%d+)")
   local num = 2^24*o1 + 2^16*o2 + 2^8*o3 + o4
@@ -23,7 +26,8 @@ function process_message()
     return 0
   end
 
-  local ip = ipv4_str_to_int(read_message("Fields[remote_addr]"))
+  local remote_addr = read_message("Fields[remote_addr]")
+  local ip = ipv4_str_to_int(remote_addr)
   local t = read_message("Timestamp")
   local cb = client_stats[ip]
 
@@ -37,15 +41,26 @@ function process_message()
   -- sum up attempt over the last 5 minutes and if more than
   -- 50, send an alert payload
   local count = 0
-  for _, v in ipairs(cb:get_range(1)) do
-    if v > 0 then
-      count = count + v
+  local vals = cb:get_range(1)
+  for i=1,5 do
+    if vals[i] > 0 then
+      count = count + vals[i]
     end
   end
-  if count > 50 then
+
+  -- if a previous violations were sent for this client, increase
+  -- the threshold to not send duplicate alerts
+  local pv = client_violations[ip]
+  if pv then
+    pv = pv + 1
+  else
+    pv = 1
+  end
+  if count > (threshold * pv) then
     add_to_payload(
       string.format("ALERT: %d repeated xss attempts from %s\n",
-        count, read_message("Fields[remote_addr]")))
+        count*pv, read_message("Fields[remote_addr]")))
+    client_violations[ip] = pv
   end
 
   -- store the circular buffer in the table
@@ -55,14 +70,17 @@ end
 
 function timer_event()
   inject_payload("txt", "xss_threshold")
-  for ip, cb in ipairs(client_stats) do
+  for ip, cb in pairs(client_stats) do
     local vals = cb:get_range(1)
     local isempty = true
-    for _, v in ipairs(vals) do
-      if v > 0 then
+    for i=1, 5 do
+      if vals[i] > 0 then
         isempty = false
       end
     end
-    if isempty then client_stats[ip] = {} end
+    if isempty then
+      client_stats[ip] = nil
+      client_violations[ip] = nil
+    end
   end
 end
